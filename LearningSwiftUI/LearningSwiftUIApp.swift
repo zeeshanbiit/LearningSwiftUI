3a204b3ac15ad2aeee9f686696d71693c752f68c
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-13 02:21:28 +0500

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Muhammad Zeshan on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
